396f69f47cef4a849d1b754bcfefb07911c7b6dd
Carrie Streed <cmstreed@gmail.com>
2016-05-31 19:11:23 -0400

    about component is set

--- app/components/About.js
+++ app/components/About.js
@@ -9,8 +9,12 @@ export default class About extends Component{
     return (
       <StyleRoot style={styles.main}>
 
-        <div>
-            {'ABOUT'}
+        <div style={styles.name}>
+            <span style={styles.carrie}>{`CARRIE`}</span>
+            <span style={styles.streed}>{`STREED`}</span>
+        </div>
+        <div style={styles.title}>
+          <p>{`web developer`}</p>
         </div>
 
       </StyleRoot>
@@ -23,6 +27,30 @@ const styles = {
     width: '100%',
     height: '400px',
     backgroundColor: 'hotpink',
+    backgroundImage: 'url(../dist/images/bgnoise1.jpg)',
+    backgroundSize: 'cover',
     color: 'black',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    fontFamily: 'Fjalla One',
   },
+  name: {
+    fontSize: '80px',
+    letterSpacing: '-3px',
+    textShadow: '0 0 10px #fff',
+  },
+  carrie: {
+    color: '#ac1a5d',
+  },
+  streed: {
+    color: 'rgba(0,0,0,.8)',
+  },
+  title: {
+    color: '#fff',
+    textShadow: '0 0 3px #000',
+    fontSize: '25px',
+    marginTop: '-35px',
+  }
 }
--- app/components/Skills.js
+++ app/components/Skills.js
@@ -22,7 +22,7 @@ const styles = {
   main: {
     width: '100%',
     height: '400px',
-    backgroundColor: 'deepPink',
+    backgroundColor: 'lightPink',
     color: 'black',
   },
 }
